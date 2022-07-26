fc1e18e5af72d7e11cc03355201d4dbbdc0cfc01
Benedykt Dryl <cypherq@gmail.com>
2022-07-26 22:42:41 +0200

    Add Chrome download to Linux

--- snap.sh
+++ snap.sh
@@ -8,8 +8,12 @@ sudo snap install yubioath-desktop
 sudo snap install slack
 sudo snap install docker
 
- sudo addgroup --system docker
- sudo adduser $USER docker
- newgrp docker
- sudo snap disable docker
- sudo snap enable docker
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
+sudo snap disable docker
+sudo snap enable docker
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
