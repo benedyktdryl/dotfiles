49e42ed885fb24b186266984a9f352b17bd364f6
Benedykt Dryl <cypherq@gmail.com>
2022-07-27 01:07:39 +0200

    Disable early exiting command

--- snap.sh
+++ snap.sh
@@ -8,11 +8,11 @@ sudo snap install yubioath-desktop
 sudo snap install slack
 sudo snap install docker
 
-sudo addgroup --system docker
-sudo adduser $USER docker
-newgrp docker
-sudo snap disable docker
-sudo snap enable docker
+#sudo addgroup --system docker
+#sudo adduser $USER docker
+#newgrp docker
+#sudo snap disable docker
+#sudo snap enable docker
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
