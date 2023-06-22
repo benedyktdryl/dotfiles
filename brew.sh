fc7fde2a7b8dc3c4ef888bf6223caedfb5b37852
Benedykt Dryl <cypherq@gmail.com>
2023-06-22 20:42:31 +0200

    Update brew.sh
    - remove Flux, Calibre
    - add qbittorent

--- brew.sh
+++ brew.sh
@@ -3,21 +3,19 @@
 # Install desktop apps with cask
 brew install --cask warp
 brew install --cask google-chrome
-brew install --cask calibre
 brew install --cask licecap
 brew install --cask visual-studio-code
 brew install --cask docker
 brew install --cask spotify
 brew install --cask vlc
 brew install --cask dbeaver-community
-brew install --cask xmind
 brew install --cask keka
-brew install --cask flux
 brew install --cask raycast
 brew install --cask signal
 brew install --cask stats
 brew install --cask slack
 brew install --cask dotnet-sdk
+brew install --cask qbittorrent
 
 # Remove outdated versions from the cellar.
 brew cleanup
